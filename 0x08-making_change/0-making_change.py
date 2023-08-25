b2ff8a0d4aea0f68f091168baeeadd0ad9bb953f
Daniel Mekonnen <101458123+DannyGetch@users.noreply.github.com>
2023-08-25 12:52:22 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,8 +4,9 @@
 
 
 def makeChange(coins, total):
-    """Given a pile of coins of different values, 
-    it determines the fewest number of coins needed to meet a given amount total
+    """Given a pile of coins of different values,
+    it determines the fewest number of coins needed
+    to meet a given amount total
     """
     if total <= 0:
         return 0
